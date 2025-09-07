3c1c6fe0dd5b183f578ed187b4d814d3f7aff71c
devyneX <ahmeds.swapno@gmail.com>
2025-09-07 19:11:57 +0600

    fix mess up

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,32 @@
 
 vim.keymap.set("i", "jj", "<esc>", { desc = "Switch to Normal Mode" })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open File Explorer" })
-vim.keymap.set("n", "[c", function()
+
+vim.keymap.set("n", "[^", function()
   require("treesitter-context").go_to_context(vim.v.count1)
 end, { silent = true })
+
+-- First line, first column
+vim.keymap.set({ 'n', 'v' }, 'gg', 'gg0', { desc = "jump to the start first line" })
+
+-- Last line, last character
+vim.keymap.set({ 'n', 'v' }, 'G', 'G$', { desc = "Jump to the end of the last line" })
+
+vim.keymap.set("n", "<leader>sf", function()
+  vim.ui.input({ prompt = "Glob pattern (e.g. *.js): " }, function(glob)
+    if not glob or glob == "" then return end
+
+    require("telescope.builtin").live_grep({
+      glob_pattern = glob,
+      prompt_title = "Live Grep in " .. glob,
+      default_text = ""
+    })
+  end)
+end, { desc = "Live grep with glob filter" })
+
+-- TODO: need to figure this out
+-- vim.keymap.set("n", "<leader>.", function()
+--   local snacks = require("lazy/snacks.nvim")
+--   snacks.opts.picker.files.hidden = true
+--   snacks.opts.explorer.files.hidden = true
+-- end, { desc = "Toggle hidden files in explorer" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.exrc = true -- Allow per-project config like `.nvim.lua`
-vim.opt.secure = true -- Prevent unsafe commands
+-- python setup
+vim.g.python3_host_prog = "/home/luffy/Documents/Dev/.uv-venv/nvim-python/bin/python"
+
+-- nodejs
+vim.g.node_host_prog = "/home/luffy/.config/nvm/versions/node/v24.5.0/bin/node"
+
+-- terminal
+vim.g.terminal_emulator = "kitty"
+vim.o.shell = "/usr/bin/zsh"
+
+vim.o.exrc = true -- Allow per-project config like `.nvim.lua`
+vim.o.secure = true -- Prevent unsafe commands
