fc7de0979fac40f596445bc99190fb6c67de4c01
devyneX <ahmeds.swapno@gmail.com>
2026-08-30 03:57:04 +0600

    clean up lsp.
    - removed fidget and added a progress using snacks instead
    - removed highlighting code. this is not needed.
    - removed mason-lspconfig and used built in lsp config.

--- lua/devynex/plugins/lsp.lua
+++ lua/devynex/plugins/lsp.lua
@@ -1,3 +1,49 @@
+function setup_lsp_progress()
+  ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+  local progress = vim.defaulttable()
+  vim.api.nvim_create_autocmd("LspProgress", {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+      local client = vim.lsp.get_client_by_id(ev.data.client_id)
+      local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+      if not client or type(value) ~= "table" then
+        return
+      end
+      local p = progress[client.id]
+
+      for i = 1, #p + 1 do
+        if i == #p + 1 or p[i].token == ev.data.params.token then
+          p[i] = {
+            token = ev.data.params.token,
+            msg = ("[%3d%%] %s%s"):format(
+              value.kind == "end" and 100 or value.percentage or 100,
+              value.title or "",
+              value.message and (" **%s**"):format(value.message) or ""
+            ),
+            done = value.kind == "end",
+          }
+          break
+        end
+      end
+
+      local msg = {} ---@type string[]
+      progress[client.id] = vim.tbl_filter(function(v)
+        return table.insert(msg, v.msg) or not v.done
+      end, p)
+
+      local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+      vim.notify(table.concat(msg, "\n"), "info", {
+        id = "lsp_progress",
+        title = client.name,
+        opts = function(notif)
+          notif.icon = #progress[client.id] == 0 and " "
+            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+        end,
+      })
+    end,
+  })
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -10,61 +56,12 @@ return {
         keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
         opts = {},
       },
-      "mason-org/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
-      { "j-hui/fidget.nvim", opts = {} },
+      -- { "j-hui/fidget.nvim", opts = {} },
       "saghen/blink.cmp",
     },
     config = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("group-lsp-attach", { clear = true }),
-        callback = function(event)
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has("nvim-0.11") == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
-
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if
-            client
-            and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
-          then
-            local highlight_augroup = vim.api.nvim_create_augroup("group-lsp-highlight", { clear = false })
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd("LspDetach", {
-              group = vim.api.nvim_create_augroup("group-lsp-detach", { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds({ group = "group-lsp-highlight", buffer = event2.buf })
-              end,
-            })
-          end
-        end,
-      })
+      setup_lsp_progress()
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
@@ -142,36 +139,21 @@ return {
             },
           },
         },
-        hyprls = {},
       }
 
       local ensure_installed = vim.tbl_keys(servers or {})
 
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-      servers = vim.tbl_deep_extend("force", servers, {
-        hyprlang = {
-          cmd = { "hyprls" },
-          settings = {
-            hyprls = {
-              preferIgnoreFile = false,
-              ignore = { "hyprlock.conf", "hypridle.conf" },
-            },
-          },
-        },
-      })
+      local excluded_servers = {}
 
-      require("mason-lspconfig").setup({
-        ensure_installed = {}, -- configured with mason-tool-installer above
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
-      })
+      for server, conf in pairs(servers) do
+        conf.capabilities = vim.tbl_deep_extend("force", {}, capabilities, conf.capabilities or {})
+        vim.lsp.config(server, conf)
+        if excluded_servers[server] == nil then
+          vim.lsp.enable(server)
+        end
+      end
     end,
   },
 }
